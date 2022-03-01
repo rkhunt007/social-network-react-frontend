af2fb1de719972838d6a68c927ec2944b362abd8
Rahul Khunt <rahul.khunt@techuplabs.com>
2022-03-01 00:05:05 -0500

    env variable setup

--- src/actions/axios.js
+++ src/actions/axios.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+  baseURL: process.env.BASE_URL || "http://localhost:5000",
 });
 
-const token = localStorage.getItem('token', '');
-instance.defaults.headers.common['x-auth-token'] = token;
+const token = localStorage.getItem("token", "");
+instance.defaults.headers.common["x-auth-token"] = token;
 
 instance.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('token', '');
-        if (token) {
-            config.headers['x-auth-token'] = token;
-        }
-        return config;
-    },
-    error => {
-        Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("token", "");
+    if (token) {
+      config.headers["x-auth-token"] = token;
     }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
 );
 
 export default instance;
